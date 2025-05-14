41752e4715e04d7517477c28be05503e150e3b89
Benjamin <bakalubabenjamin@gmail.com>
2025-05-15 02:09:45 +0300

    added into the views.py file home, course_list, student_list

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Student,Course,Teacher
 
 # Create your views here.
+def home(request):
+    return render(request, 'core/home.html')
+
+def course_list(request):
+    courses = Course.objects.all()
+    return render(request, 'core/course_list.html', {'courses': courses})
+
+def student_courses(request, student_id):
+    student =Student.objects.get(student_id = student_id)
+    courses = Student.courses.all()
+    return render(request, 'core/student_courses.html', {'student': student, 'courses': courses})

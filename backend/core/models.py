4d6b0b5316e6fab4fed50eea3022f4b2c9c0457a
Benjamin <bakalubabenjamin@gmail.com>
2025-05-15 01:42:56 +0300

    initial set up of the student system, adding of models

---
+++ backend/core/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+
+# Create your models here.
+class Teacher(models.Model):
+    name = models.CharField(max_length=100)
+    employee_id = models.IntegerField(unique=True)
+    email =models.EmailField(unique=True)
+
+    def _str_(self):
+        return self.name
+    
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, related_name='courses')
+    students = models.ManyToManyField('Student', related_name='courses')
+
+    def __str__(self):
+        return self.title
+    
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    student_id = models.IntegerField(unique=True)
+    grade = models.IntegerField()
+    enrollment_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+    
+class Attendance(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    date = models.DateField()
+    is_present = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.course.title} - {self.date}"
+    
+class Fee(models.Model):
+    student = models.ForeignKey(Student, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    due_date = models.BooleanField(default=False)
+    is_paid = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.student.name} - {self.amount}"
+
+
+    
+

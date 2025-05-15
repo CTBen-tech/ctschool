f64afd85437a550534dc491ae83e92b00dce2e4c
Benjamin <bakalubabenjamin@gmail.com>
2025-05-15 14:58:10 +0300

    createsd abstract user and also added abstract class to models and updtaed settings by adding Auth_User _Model to settings

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    is_teacher = models.BooleanField(default=False)
+    is_student = models.BooleanField(default=False)
+    
 class Teacher(models.Model):
     name = models.CharField(max_length=100)
     employee_id = models.IntegerField(unique=True)
